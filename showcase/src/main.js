b9377502472cc95be7ea6ec847b9827384cbbfdd
Maxence Leroy <maxence.leroy@epitech.eu>
2024-09-26 09:29:23 +0200

    feat(translation): add translation method with csv

--- showcase/src/main.js
+++ showcase/src/main.js
@@ -1,10 +1,34 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
-import './main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './main.css';
 
-const app = createApp(App)
+import { createI18n } from 'vue-i18n';
+import translations from './assets/translations.csv'; // Import direct du fichier CSV
+
+// Créer un objet pour stocker les messages de traduction
+let messages = {};
+
+// Parcourir les données importées du CSV
+translations.forEach(row => {
+  Object.keys(row).forEach(lang => {
+    if (lang !== 'tag') {
+      if (!messages[lang]) messages[lang] = {};
+      messages[lang][row.tag] = row[lang];
+    }
+  });
+});
+
+// Configurer Vue I18n
+const i18n = createI18n({
+  locale: 'en', // Langue par défaut
+  messages,     // Les traductions depuis le CSV
+});
+
+// Créer et monter l'application Vue
+const app = createApp(App);
 
 app
-    .use(router)
-    .mount('#app')
+  .use(router)
+  .use(i18n)
+  .mount('#app');
--- showcase/vue.config.js
+++ showcase/vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.csv$/,
+          use: [
+            {
+              loader: 'csv-loader',
+              options: {
+                dynamicTyping: true,
+                header: true,
+                skipEmptyLines: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+});
