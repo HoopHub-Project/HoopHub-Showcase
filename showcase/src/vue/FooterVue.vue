3d1331b837022a7284af1639f041930fa527615a
Maxence Leroy <maxence.leroy@epitech.eu>
2024-09-27 10:39:38 +0200

    feat(develop): add vue folder and component folder and initialize the header content and footer repartition

---
+++ showcase/src/vue/FooterVue.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Footer</div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterVue',
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ showcase/src/vue/HeaderVue.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Header</div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderVue',
+    }
+</script>
+
+<style scoped>
+
+</style>
