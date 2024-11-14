a6be14517725510e53fde1984992a45f5809233c
Maxence Leroy <maxence.leroy@epitech.eu>
2024-11-14 10:47:37 +0100

    feat(svt-19-app-pres-detail): add training component for the application presentation

---
+++ showcase/src/components/Application/TrainingVue.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="training-container">
+
+        <div class="training-title">
+                <h1>{{ $t('training_hoophub') }}</h1>
+            </div>
+            <div class="training-content">
+                <p>{{ $t('training_hoophub_content') }}</p>
+            </div>
+            <div class="training-image-container">
+                <img src="@/assets/Logos/logo_HoopHub.png" alt="HoopHub Logo" :title="$t('hoophub')" />
+            </div> 
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TrainingVue'
+}
+</script>
+
+<style scoped>
+
+.training-container {
+    display: flex;
+    flex-direction: column;
+    padding: 50px 50px 0px 50px;
+    background-color: var(--hoophub-background);
+}
+
+.training-title {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.training-title h1 {
+    font-size: 48px;
+    font-weight: 600;
+    margin-bottom: 10px;
+    line-height: 58px;
+}
+
+.training-content {
+    display: flex;
+    flex-direction: column;
+}
+
+.training-image-container {
+    display: flex;
+    justify-content:end;
+    align-items: center;
+    margin-top: 50px;
+}
+
+.training-image-container img {
+    width: 200px;
+}
+
+@media (max-width: 768px) {
+    
+    .training-container {
+        padding: 50px 20px 0px 20px;
+    }
+
+    .training-title h1 {
+        font-size: 40px;
+        line-height: 54px;
+    }
+
+    .training-button-container {
+        align-items: center;
+    }
+
+    .training-image-container img {
+        width: 110px;
+    }
+
+}
+
+</style>
--- showcase/src/vue/ApplicationVue.vue
+++ showcase/src/vue/ApplicationVue.vue
@@ -1,17 +1,20 @@
 <template>
     <ApplicationTaglineVue />
     <WhatIsHoopHubVue />
+    <TrainingVue />
 </template>
 
 <script>
 import ApplicationTaglineVue from '@/components/Application/ApplicationTaglineVue.vue';
 import WhatIsHoopHubVue from '@/components/Application/WhatIsHoopHubVue.vue';
+import TrainingVue from '@/components/Application/TrainingVue.vue';
 
 export default {
   name: 'ApplicationVue',
   components: {
     ApplicationTaglineVue,
-    WhatIsHoopHubVue
+    WhatIsHoopHubVue,
+    TrainingVue
   }
 }
 </script>
